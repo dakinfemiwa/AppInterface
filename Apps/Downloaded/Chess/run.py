75fd6daa5a40a12f8c8a5a8b86172eb62f8cecf9
dakinfemiwa <daveak1602@gmail.com>
2019-09-03 18:37:30 +0100

    Add files via upload

---
+++ Apps/Downloaded/Chess/run.py
@@ -0,0 +1,13 @@
+import tkinter
+from tkinter imoprt *
+import random
+
+class run:
+    def __init__(self, AppScreen, AppName, width, height):
+        self.AppScreen = AppScreen
+        self.AppName = AppName
+        self.screenWidth = width
+        self.screenHeight = height
+        print(self.AppName)
+
+
